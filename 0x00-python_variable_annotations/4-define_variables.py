9349e77c4b5e61475553ec9e95e766732db8aec4
Mark-Tevin <tevinsmark@gmail.com>
2024-10-28 11:53:02 +0000

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Define and annotate the following variables with specific values
+a, and int with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with value of True
+school, a str with a value of "Holberton"
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
